fab8589b8d12db588ef16f61dbc62cd176e1a9ef
SEN-SEI <SEN-SEI@Suneel>
2023-11-08 13:17:55 +0530

    redmime update

---
+++ src/Samasta/Redmine_edit_effort_update.java
@@ -0,0 +1,58 @@
+package Samasta;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class Redmine_edit_effort_update {
+
+	static String url = "https://new-redmine5.senseitech.com/";
+	static String username = "suneel.kumar@sen-sei.in";
+	static String password = "Suneel@2021";
+	static WebDriver driver;
+
+	@BeforeTest
+	public static void launch_browser() {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get(url);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+//		Thread.sleep(3000);
+		driver.findElement(By.name("username")).sendKeys(username);
+		driver.findElement(By.name("password")).sendKeys(password);
+		driver.findElement(By.name("login")).click();
+
+		driver.findElement(By.id("project-jump")).click();
+		driver.findElement(By.xpath("//*[@title='Samasta']")).click();
+	}
+
+	@Test
+	public void update_efforts() {
+
+		driver.findElement(By.xpath("//*[@class='sprint']")).click();
+
+		driver.findElement(By.xpath("//*[@title='Edit effort']")).click();
+
+		List<String> user = Arrays.asList(/* Anil */"15", /* Dharm */"16", /* Bhargavi */ "17", /* Pinky */"18",
+				/* Suneel */"19", /* Sharmila */ "28", /* Shijas "40",*/  /* Sima */"44");
+		for (int j = 0; j < user.size(); j++) {
+
+			for (int i = 0; i < 12; i++) {
+//				driver.findElement(By.id("user_" + user.get(j) + "_" + i)).clear();
+				if (i != 5 && i != 6) {
+
+					driver.findElement(By.id("user_" + user.get(j) + "_" + i)).sendKeys("8");
+				}
+			}
+		}
+
+	}
+
+}
---
+++ src/Samasta/Redmine_status_update.java
@@ -0,0 +1,70 @@
+package Samasta;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Redmine_status_update {
+
+	String subject = "07-11-2023";
+	String comments = "1. Team meeting 2. Audit close from the backend 3. Center not showing";
+
+	@BeforeTest
+	public void lb() {
+		Redmine_edit_effort_update.launch_browser();
+	}
+
+	@Test(priority = 1)
+	public void create_issue() {
+		Redmine_edit_effort_update.driver.findElement(By.id("new-object")).click();
+
+		Redmine_edit_effort_update.driver.findElement(By.xpath("//a[text()='New issue']")).click();
+
+		Select Tracker = new Select(Redmine_edit_effort_update.driver.findElement(By.id("issue_tracker_id")));
+
+		Tracker.selectByVisibleText("Support");
+
+		Redmine_edit_effort_update.driver.findElement(By.id("issue_subject"))
+				.sendKeys("Production support on " + subject);
+
+		Redmine_edit_effort_update.driver.findElement(By.id("issue_parent_issue_id")).sendKeys("363");
+
+		Redmine_edit_effort_update.driver.findElement(By.xpath("//*[@data-id=19]")).click();
+
+		Select Done = new Select(Redmine_edit_effort_update.driver.findElement(By.id("issue_done_ratio")));
+		Done.selectByValue("100");
+
+		try {
+
+			Select sprint = new Select(Redmine_edit_effort_update.driver.findElement(By.id("issue_sprint_id")));
+			List<WebElement> options = sprint.getOptions();
+			sprint.selectByIndex(options.size() - 1);
+		} catch (Exception e) {
+			Select sprint = new Select(Redmine_edit_effort_update.driver.findElement(By.id("issue_sprint_id")));
+			List<WebElement> options = sprint.getOptions();
+			sprint.selectByIndex(options.size() - 1);
+		}
+
+		Redmine_edit_effort_update.driver.findElement(By.xpath("//*[@value='Create']")).click();
+
+	}
+
+	@Test(priority = 2)
+	public void log_time() {
+		Redmine_edit_effort_update.driver.findElement(By.xpath("//*[@class='icon icon-time-add']")).click();
+
+		Redmine_edit_effort_update.driver.findElement(By.id("time_entry_hours")).sendKeys("8");
+
+		Redmine_edit_effort_update.driver.findElement(By.id("time_entry_comments")).sendKeys(comments);
+
+		Select Activity = new Select(Redmine_edit_effort_update.driver.findElement(By.id("time_entry_activity_id")));
+		Activity.selectByVisibleText("Support");
+
+		Redmine_edit_effort_update.driver.findElement(By.xpath("//*[@value='Create']")).click();
+	}
+
+}
